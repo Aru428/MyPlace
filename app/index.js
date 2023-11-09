cdb0e1d231973651af669b3ffb5663af2d72bb4c
ldw0123 <ldw0123@gmail.com>
2023-11-09 17:05:14 +0900

    [FEAT] - index.js 파일 생성

--- app/index.js
+++ app/index.js
@@ -1,9 +1,24 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 const PORT = 8000;
 
-app.get("/", function (req, res) {
-  res.send("Hello project");
+app.set('view engine', 'ejs');
+app.use('/static', express.static(__dirname + '/static'));
+
+app.get('/', function (req, res) {
+  res.send('Hello project');
+});
+
+app.get('/signin', function (req, res) {
+  res.render('signin');
+});
+
+app.get('/signup', function (req, res) {
+  res.render('signup');
+});
+
+app.get('/editProfile', function (req, res) {
+  res.render('editProfile');
 });
 
 app.listen(PORT, function () {
