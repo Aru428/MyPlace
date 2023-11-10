dc5ad8551b725b0d68e32800c2560bb77498c3d3
yoonbung12 <84695780+yoonbung12@users.noreply.github.com>
2023-11-10 15:13:14 +0900

    FEAT-갤러리 테이블 추가

---
+++ app/model/Gallery.js
@@ -0,0 +1,41 @@
+function Gallery(Sequelize, DataTypes) {
+    return Sequelize.define(
+        "Gallery",
+        {
+            // g_id, g_name, address,deadline, website, category
+            g_id: {
+                type: DataTypes.INT,
+                allowNull: false,
+                primaryKey: true,
+                autoIncrement: true,
+            },
+            g_name: {
+                type: DataTypes.STRING(20),
+                allowNull: false,
+            },
+            address: {
+                type: DataTypes.STRING(50),
+                allowNull: false,
+            },
+            deadline: {
+                type: DataTypes.DATE,
+                allowNull: false,
+            },
+            website: {
+                type: DataTypes.STRING(30),
+                allowNull: false,
+            },
+            category: {
+                type: DataTypes.STRING(10),
+                allowNull: false,
+            },
+        },
+        {
+            tableName: "Gallery",
+            freezeTableName: true,
+            timestamps: false,
+        }
+    );
+}
+
+module.exports = Gallery;
