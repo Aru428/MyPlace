6bfa5a690ef28ca9c680717ae7cd3637c70d1798
yoonbung12 <84695780+yoonbung12@users.noreply.github.com>
2023-11-11 15:45:12 +0900

    [Feat]-signin 컨트롤러

--- app/controller/Csignin.js
+++ app/controller/Csignin.js
@@ -1,5 +1,22 @@
+const {User} = require("../model/index")
 exports.loginPage = (req, res) => {
   res.render("signin");
 };
 
-exports.postLogin = (req, res) => {};
+exports.postLogin = async(req, res) => {
+  User.findOne({
+    where: {
+      // 컬럼명 : 사용자가 적은값
+      u_id: req.body.u_id,
+      password: req.body.password,
+    },
+  }).then((result) => {
+    console.log("findOne: ", result);
+    // session 적용전
+    if(result) {
+      res.send({result: true}); 
+    } else {
+      res.send({result: false});
+    }
+  })
+};
