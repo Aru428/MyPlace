b744a74d602310cad62d52e887c45593b1d399cf
yoonbung12 <84695780+yoonbung12@users.noreply.github.com>
2023-11-14 17:30:09 +0900

    [FEAT]-로그아웃 기능구현

--- app/routes/index.js
+++ app/routes/index.js
@@ -12,6 +12,10 @@ const signin = require("../controller/Csignin");
 userRouter.get("/signin", signin.loginPage);
 userRouter.post("/signin", signin.postLogin);
 
+// 로그아웃 관련
+const logout = require("../controller/Cmain");
+mainRouter.post("/logout", main.logOut);
+
 // 회원가입 관련
 const signup = require("../controller/Csignup");
 userRouter.get("/signup", signup.signUpPage);
@@ -27,6 +31,8 @@ userRouter.get("/profile/edit", profile.profilePage);
 userRouter.patch("/profile/edit/:u_id", profile.profileEdit);
 userRouter.get("/profile/delete", profile.deleteCheckPage);
 userRouter.delete("/profile/delete/:u_id", profile.profileDelete);
+
+
 
 // 카테고리 관련
 const category = require("../controller/Clist");
