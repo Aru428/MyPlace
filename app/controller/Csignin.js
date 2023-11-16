acbe22c8f5c20d9adb1455791599d6d366470559
ldw0123 <ldw0123@gmail.com>
2023-11-16 22:15:20 +0900

    [FEAT] - map ui 수정

--- app/controller/Csignin.js
+++ app/controller/Csignin.js
@@ -1,9 +1,9 @@
-const {User} = require("../model/index")
+const { User } = require('../model/index');
 exports.loginPage = (req, res) => {
-  res.render("signin");
+  res.render('signin');
 };
 
-exports.postLogin = async(req, res) => {
+exports.postLogin = async (req, res) => {
   User.findOne({
     where: {
       // 컬럼명 : 사용자가 적은값
@@ -11,19 +11,19 @@ exports.postLogin = async(req, res) => {
       password: req.body.password,
     },
   }).then((result) => {
-    console.log("findOne: ", result);
+    console.log('findOne: ', result);
     // session 적용전
     // if(result) {
-    //   res.send({result: true}); 
+    //   res.send({result: true});
     // } else {
     //   res.send({result: false});
     // }
 
-    if(result) {
-      req.session.user = result.u_id
-      res.send({result: true, id: result.u_id});
+    if (result) {
+      req.session.user = result.u_id;
+      res.send({ result: true, id: result.u_id });
     } else {
-      res.send({result: false});
+      res.send({ result: false });
     }
-  })
+  });
 };
