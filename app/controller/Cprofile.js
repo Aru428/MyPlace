cb30aaa8d9ebcc537fac6eda065862867ad7eec7
yoonbung12 <84695780+yoonbung12@users.noreply.github.com>
2023-11-20 19:25:36 +0900

    [FEAT]-회원 탈퇴, 회원 수정 폼으로까지이동

--- app/controller/Cprofile.js
+++ app/controller/Cprofile.js
@@ -1,31 +1,55 @@
 const { User } = require("../model");
-
+const pwSalt = require("../model/pwSalt");
 // 회원 수정/탈퇴 선택 페이지 관련
 exports.selectProfilePage = (req, res) => {
   res.render("editProfile_select");
 };
 
 // 회원 수정 페이지 관련
-
 exports.editCheckPage = (req, res) => {
   // 수정 시 비밀번호 확인 페이지 렌더
+  console.log('req.session.user:::', req.session.user);
   res.render("editProfile_editCheck");
 };
 
 exports.editCheckPw = (req, res) => {
-  // 수정 전 비밀번호 확인
+  // ---------------- 암호화 적용후
+  console.log("req.session.user: ", req.session.user);
+  
   User.findOne({
     where: {
       u_id: req.session.user,
-      password: req.body.password,
     },
-  }).then((row) => {
-    if (row) {
-      res.send({ result: true });
+  }).then((result) => {
+    console.log("result 값좀 찍어줘:",result);
+
+    if(result) {
+      pwSalt
+            .comparePassword(
+              req.body.password,
+              result.salt,
+              result.password
+            )
+            .then((pwCorrect) => {
+              if(pwCorrect) {
+                console.log("pwCorrect", pwCorrect);
+                res.send({result: true});
+              } else {
+                res.send({ result: false});
+              }
+            })
+              .catch((error) => {
+                console.log('password에러 입니다.', error);
+                res.send({result: false});
+              }); 
     } else {
-      res.send({ result: false });
-    }
-  });
+      res.send({ result: false});
+    } 
+  }).catch((error) => {
+    console.log("유저의 존재를 못찾겠습니다.", error);
+    res.send({result: false});
+  })
+  
 };
 
 exports.profilePage = (req, res) => {
@@ -34,12 +58,14 @@ exports.profilePage = (req, res) => {
     where: {
       u_id: req.session.user,
     },
-  }).then((row) => {
+  }).then((result) => {
+    console.log("result ==", result);
     const data = {
-      id: row.u_id,
-      pw: row.password,
-      name: row.name,
-      email: row.email,
+      id: result.u_id,
+      email: result.email,
+      pw: result.password,
+      name: result.name,
+
     };
     res.render("editProfile", { data: data });
   });
@@ -55,6 +81,7 @@ exports.profileEdit = (req, res) => {
       u_id: req.session.user,
     },
   }).then((row) => {
+    console.log("row:", row);
     if (row) {
       res.send({ result: true });
     } else {
@@ -84,32 +111,49 @@ exports.profileAllEdit = (req, res) => {
 
 // 회원 탈퇴 시 비밀번호 확인 페이지 관련
 exports.deleteCheckPage = (req, res) => {
+  console.log("req.session.user:::::::::", req.session.user);
   res.render("editProfile_delete");
 };
 
 // 회원 탈퇴
 exports.userDelete = (req, res) => {
+  console.log("req.session.user", req.session.user);
   User.findOne({
     where: {
-      u_id: req.session.user,
-      password: req.body.password,
+      u_id : req.session.user,
     },
-  }).then((row) => {
-    console.log("결과", row);
-    if (row) {
-      User.destroy({
-        where: {
-          u_id: row.u_id,
-        },
-      }).then((result) => {
-        req.session.destroy((err) => {
-          if (err) throw err;
+  }).then((result) => {
+    if(result) {
+      pwSalt
+            .comparePassword(
+              req.body.password,
+              result.salt,
+              result.password,
+            ).then((pwCorrect) =>{
+              if(pwCorrect) {
+                User.destroy({
+                  where: {
+                    u_id: result.u_id,
+                  },
+                }).then((result) => {
+                  req.session.destroy((err) =>{
+                    if(err) throw err;
+                  })
+                })
+                res.send({result: true});
 
-          res.send({ result: true });
-        });
-      });
-    } else {
-      res.send({ result: false });
-    }
+              } else {
+                res.send({result: false});
+              }
+            }).catch((error) => {
+              console.log("pw ERROR", error);
+              res.send({result: false});
+            });
+     } else {
+       res.send({result: false});
+     }
+  }).catch((error) => {
+    console.log('no user:' , error);
+    res.send({result: false});
   });
 };
